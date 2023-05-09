88d9bfc4f9cd52fb77c1f9eec490cec8e2951791
ditdevops1 <ditdevosp1@gmail.com>
2023-05-09 11:07:18 +0000

    ajout de la division avec erreur d'affichage

--- somme.py
+++ somme.py
@@ -6,9 +6,14 @@ nbr2 = input('Entrez le deuxième nombre: ')
 s = int(nbr1) + int(nbr2)
 # Multiplication
 m=int(nbr1) * int(nbr2)
+# Division
+d=int(nbr1) / int(nbr2)
 
 # Afficher la somme
 print('La somme de {0} et  {1} est {2}'.format(nbr1, nbr2, s))
 
 # Afficher la multiplication
 print('La multiplication de {0} et  {1} est {2}'.format(nbr1, nbr2, m))
+
+# Afficher la divison avec erreur pour testet le CI
+print('La multiplication de {0} et  {1} est {2}'.format(nbr1, nbr2, d
